c1b12ee8013fa4044e932c6e15fe302553dcbbbb
Palak09sh <palakbhardwaj195@gmail.com>
2025-09-22 14:36:19 +0530

    feat: learned navigation,Link, layouts, assets, metadata & CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,18 @@
-export default function FirstPost() {
-    return(
-        <h1>First Post</h1>
+import Link from 'next/link';
+import Head from 'next/head'
+import Layout from '../component/layout';
+export default function FirstPost(){
+    return (
+        <Layout>
+         <Head>
+            <title>
+                First Post
+            </title>
+         </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+   </Layout>
     )
 }
