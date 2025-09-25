0187f2f478441730021daec5d1c0852006759ec3
Palak09sh <palakbhardwaj195@gmail.com>
2025-09-25 10:06:49 +0530

    feat: learn and implement the static pre-rendering using (getStaticProps function)

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,35 @@
 import Head from 'next/head';
 import Layout, {siteTitle} from '../pages/component/layout';
 import ustilStyles from '../styles/utils.module.css';
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts';
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    // returning allPostsData inside the props object in getStaticProps, the blog posts will be passed to the Home component as a prop. 
+    props: {
+      allPostsData,
+    },
+}
+}
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={ustilStyles.headingMd}>
+      <section className={`{$ustilStyles.headingMd} ${ustilStyles.padding1px}`}  >
+        <h2 className={ustilStyles.headingLg}>Blog</h2>
+        <ul className={ustilStyles.list}>
+          {allPostsData.map(({id,date,title})=> (
+             <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
         <p>Hey! I am Palak Sharma. A 3rd year CSE student bulding frontend. </p>
         <p>
           this is the sample website.
--- pages/lib/posts.js
+++ pages/lib/posts.js
@@ -3,23 +3,26 @@ import path from 'path';
 import matter from 'gray-matter';
 const postDirectory = path.join(process.cwd(),'posts')
 // process.cwd() method returns the current working directory of the Node.js process.
-export default function getSortedPostData() {
-const fileName = fs.readdirSync(postsDirectory);
-const allPostsData = fileNames.map((fileName)=>{
-    const id = fileName.replace(/\.md$/,'')
-    const fullPath = path.join(postsDirectory,fileName);
-const fileContents = fs.readFileSync(fullPath,'utf8')
-const matterResult = matter(fileContents)
-return{
-    ...matterResult.data,
-}
-})
-return allPostsData.sort((a,b) => {if(a.date<b.date)
-{
-    return 1;
-}
-else{
-    return -1;
-}
-})
+export async function getSortedPostData() {
+    // this is to fetch the data from external endpoint
+const res = await fetch('');
+return res.json();
+// const fileNames = fs.readdirSync(postsDirectory);
+// const allPostsData = fileNames.map((fileName)=>{
+//     const id = fileName.replace(/\.md$/,'')
+//     const fullPath = path.join(postsDirectory,fileName);
+// const fileContents = fs.readFileSync(fullPath,'utf8')
+// const matterResult = matter(fileContents)
+// return{
+//     ...matterResult.data,
+// }
+// })
+// return allPostsData.sort((a,b) => {if(a.date<b.date)
+// {
+//     return 1;
+// }
+// else{
+//     return -1;
+// }
+// })
 }
