c193fd08840ef7f7dd16c85d08a6e457b8ba048a
Palak09sh <palakbhardwaj195@gmail.com>
2025-09-29 12:00:45 +0530

    polish index and posts page

--- pages/component/date.js
+++
@@ -1,5 +0,0 @@
-import { parserISO, format }from 'date-fns';
-export default function Date({ dateString }) {
-    const date = parserISO(dateString)
-    return<time dateTime = {dateString}>{format(date,'LLLL d, yyyy')}</time>
-}
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,26 +3,24 @@ import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
 import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css';
+
 export default function Post({ postData }) {
   return (
     <Layout>
       <Head>
-        <title>  {postData.title}
-
-        </title>
+        <title>{postData.title}</title>
       </Head>
       <article>
-        <h1 className={utilStyles.headingX1}>{postData.title}</h1>
-       <div className={utilStyles.lightText}>
-           <Date dateString = {postData.date} />
-       </div>
-    
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml}} />
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
-     
     </Layout>
   );
 }
+
 export async function getStaticPaths() {
   const paths = getAllPostIds();
   return {
@@ -30,8 +28,9 @@ export async function getStaticPaths() {
     fallback: false,
   };
 }
+
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
